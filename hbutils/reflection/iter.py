2b83254694ff59da49d51c7263cced26fc8bbd79
HansBug <hansbug@buaa.edu.cn>
2022-02-06 11:16:10 +0800

    dev(for): add progressive_for function

--- hbutils/reflection/iter.py
+++ hbutils/reflection/iter.py
@@ -1,12 +1,12 @@
-from itertools import tee
-from typing import Iterator, Iterable
+from itertools import tee, chain, islice
+from typing import Iterator, Iterable, Tuple
 
 __all__ = [
-    'nested_for',
+    'nested_for', 'progressive_for',
 ]
 
 
-def nested_for(*iters: Iterable) -> Iterator:
+def nested_for(*iters: Iterable) -> Iterator[Tuple[object, ...]]:
     """
     Nested for based on several iterators.
 
@@ -48,3 +48,83 @@ def nested_for(*iters: Iterable) -> Iterator:
                 selections.pop()
 
     yield from _recursion(0, [])
+
+
+def _yield_progressive_for(iterable: Iterable, n: int, offset: int) -> Iterator:
+    def _recursion(deep, iters, selections: list):
+        if deep >= n:
+            yield tuple(selections)
+        else:
+            iters[0], new_iter = tee(iters[0])
+            if deep > 0:
+                actual_iter = islice(chain(selections[-1:], new_iter), offset, None)
+            else:
+                actual_iter = new_iter
+
+            while True:
+                try:
+                    item = next(actual_iter)
+                except (StopIteration, StopAsyncIteration):
+                    break
+
+                selections.append(item)
+                iter_proxy = [actual_iter]
+                yield from _recursion(deep + 1, iter_proxy, selections)
+                (actual_iter,) = iter_proxy
+                selections.pop()
+
+    yield from _recursion(0, [iterable], [])
+
+
+def progressive_for(iterable: Iterable, n: int, offset: int = 1) -> Iterator[Tuple[object, ...]]:
+    """
+    Progressive for based on one given ``iterable``.
+
+    :param iterable: Iterable object for this loop.
+    :param n: Depth of this loop.
+    :param offset: Offset of this loop, default is ``1`` which means the first value \
+        in the next level will be the one after the above level.
+    :returns: Progressive for loop iteration.
+
+    Examples::
+        >>> from hbutils.reflection import progressive_for
+        >>> for a, b in progressive_for(range(4), 2):
+        ...     print(a, b)
+        0 1
+        0 2
+        0 3
+        1 2
+        1 3
+        2 3
+        >>> for a, b in progressive_for(range(4), 2, 0):
+        ...     print(a, b)
+        0 0
+        0 1
+        0 2
+        0 3
+        1 1
+        1 2
+        1 3
+        2 2
+        2 3
+        3 3
+        >>> for a, b, c in progressive_for(range(5), 3):
+        ...     print(a, b, c)
+        0 1 2
+        0 1 3
+        0 1 4
+        0 2 3
+        0 2 4
+        0 3 4
+        1 2 3
+        1 2 4
+        1 3 4
+        2 3 4
+        >>> for a, b, c in progressive_for(range(5), 3, 2):
+        ...     print(a, b, c)
+        0 2 4
+    """
+    if offset < 0:
+        raise ValueError(f'Offset for progressive_for should not be lower than 0, but {repr(offset)} found.')
+
+    return _yield_progressive_for(iterable, n, offset)
--- test/reflection/test_iter.py
+++ test/reflection/test_iter.py
@@ -1,6 +1,6 @@
 import pytest
 
-from hbutils.reflection import nested_for
+from hbutils.reflection import nested_for, progressive_for
 
 
 @pytest.mark.unittest
@@ -23,3 +23,42 @@ class TestReflectionIter:
             (3, 'a', 1), (3, 'a', 4), (3, 'a', 9),
             (3, 'b', 1), (3, 'b', 4), (3, 'b', 9),
         ]
+
+    def test_progressive_for(self):
+        assert list(progressive_for(map(lambda x: x ** 2, range(1, 6)), 3)) == [
+            (1, 4, 9), (1, 4, 16), (1, 4, 25), (1, 9, 16), (1, 9, 25), (1, 16, 25),
+            (4, 9, 16), (4, 9, 25), (4, 16, 25),
+            (9, 16, 25),
+        ]
+        assert list(progressive_for(map(lambda x: x ** 2, range(1, 6)), 3, 1)) == [
+            (1, 4, 9), (1, 4, 16), (1, 4, 25), (1, 9, 16), (1, 9, 25), (1, 16, 25),
+            (4, 9, 16), (4, 9, 25), (4, 16, 25),
+            (9, 16, 25),
+        ]
+        assert list(progressive_for(map(lambda x: x ** 2, range(1, 6)), 3, 0)) == [
+            (1, 1, 1), (1, 1, 4), (1, 1, 9), (1, 1, 16), (1, 1, 25),
+            (1, 4, 4), (1, 4, 9), (1, 4, 16), (1, 4, 25),
+            (1, 9, 9), (1, 9, 16), (1, 9, 25),
+            (1, 16, 16), (1, 16, 25),
+            (1, 25, 25),
+
+            (4, 4, 4), (4, 4, 9), (4, 4, 16), (4, 4, 25),
+            (4, 9, 9), (4, 9, 16), (4, 9, 25),
+            (4, 16, 16), (4, 16, 25),
+            (4, 25, 25),
+
+            (9, 9, 9), (9, 9, 16), (9, 9, 25),
+            (9, 16, 16), (9, 16, 25),
+            (9, 25, 25),
+
+            (16, 16, 16), (16, 16, 25),
+            (16, 25, 25),
+
+            (25, 25, 25),
+        ]
+        assert list(progressive_for(map(lambda x: x ** 2, range(1, 6)), 3, 2)) == [
+            (1, 9, 25)
+        ]
+
+        with pytest.raises(ValueError):
+            progressive_for(map(lambda x: x ** 2, range(1, 6)), 3, -1)
