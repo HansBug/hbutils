cfcbcfd7634e0d816fdc1cd572fa83c013d1ad39
HansBug <hansbug@buaa.edu.cn>
2022-03-01 13:21:09 +0800

    dev(hansbug): upgrade nested_map function

--- hbutils/collection/functional.py
+++ hbutils/collection/functional.py
@@ -2,6 +2,8 @@
 Overview:
     Function operations for nested structure.
 """
+from ..reflection import dynamic_call
+
 __all__ = [
     'nested_map'
 ]
@@ -26,10 +28,25 @@ def nested_map(f, s):
         ...     {'a': 3, 'b': (4, 5)},
         ... ])
         [3, 4, (5, {'x': 3, 'y': 5}), {'a': 4, 'b': (5, 6)}]
+        >>> nested_map(lambda x, p: (x + 1) *  len(p), [
+        ...     2, 3, (4, {'x': 2, 'y': 4}),
+        ...     {'a': 3, 'b': (4, 5)},
+        ... ])
+        [3, 4, (10, {'x': 9, 'y': 15}), {'a': 8, 'b': (15, 18)}]
+        >>> nested_map(lambda: 233, [
+        ...     2, 3, (4, {'x': 2, 'y': 4}),
+        ...     {'a': 3, 'b': (4, 5)},
+        ... ])
+        [233, 233, (233, {'x': 233, 'y': 233}), {'a': 233, 'b': (233, 233)}]
     """
-    if isinstance(s, dict):
-        return type(s)({k: nested_map(f, v) for k, v in s.items()})
-    elif isinstance(s, (list, tuple)):
-        return type(s)(nested_map(f, v) for v in s)
-    else:
-        return f(s)
+    _df = dynamic_call(f)
+
+    def _recursion(sval, p):
+        if isinstance(sval, dict):
+            return type(sval)({k: _recursion(v, (*p, k)) for k, v in sval.items()})
+        elif isinstance(sval, (list, tuple)):
+            return type(sval)(_recursion(v, (*p, i)) for i, v in enumerate(sval))
+        else:
+            return _df(sval, p)
+
+    return _recursion(s, ())
