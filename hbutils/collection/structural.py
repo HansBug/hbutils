9e207a4e926f4ab5b3b6b3d234f197cda2489238
HansBug <hansbug@buaa.edu.cn>
2022-03-01 11:21:14 +0800

    dev(hansbug): add nested_flatten and nested_walk

--- hbutils/collection/structural.py
+++ hbutils/collection/structural.py
@@ -2,8 +2,11 @@
 Overview:
     Structural operations.
 """
+from typing import Iterator, Tuple, Union, List
+
 __all__ = [
     'sq_flatten',
+    'nested_walk', 'nested_flatten',
 ]
 
 
@@ -31,3 +34,59 @@ def sq_flatten(s):
         [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
     """
     return list(_g_sq_flatten(s))
+
+
+def _nested_walk(s, path):
+    if isinstance(s, dict):
+        for k, v in s.items():
+            yield from _nested_walk(v, (*path, k))
+    elif isinstance(s, (list, tuple)):
+        for i, v in enumerate(s):
+            yield from _nested_walk(v, (*path, i))
+    else:
+        yield path, s
+
+
+def nested_walk(s) -> Iterator[Tuple[Tuple[Union[int, str], ...], object]]:
+    """
+    Overview:
+        Walk for nested structure.
+
+    Arguments:
+        - s: Given structure.
+
+    Returns:
+        - walk_iter (:obj:`Iterator[Tuple[Tuple[Union[int, str], ...], object]]`): Iterator of the walk result.
+
+    Examples::
+        >>> from hbutils.collection import nested_walk
+        >>> for p, v in nested_walk({'a': 1, 'b': ['c', 'd', {'x': (3, 4), 'y': 'f'}]}):
+        ...     print(p, v)
+        ...
+        ('a',) 1
+        ('b', 0) c
+        ('b', 1) d
+        ('b', 2, 'x', 0) 3
+        ('b', 2, 'x', 1) 4
+        ('b', 2, 'y') f
+    """
+    yield from _nested_walk(s, ())
+
+
+def nested_flatten(s) -> List[Tuple[Tuple[Union[int, str], ...], object]]:
+    """
+    Overview:
+        Flatten for nested structure.
+
+    Arguments:
+        - s: Given structure.
+
+    Returns:
+        - flatted (:obj:`List[Tuple[Tuple[Union[int, str], ...], object]]`): List of the flatted result.
+
+    Examples::
+        >>> from hbutils.collection import nested_flatten
+        >>> print(nested_flatten({'a': 1, 'b': ['c', 'd', {'x': (3, 4), 'y': 'f'}]}))
+        [(('a',), 1), (('b', 0), 'c'), (('b', 1), 'd'), (('b', 2, 'x', 0), 3), (('b', 2, 'x', 1), 4), (('b', 2, 'y'), 'f')]
+    """
+    return list(nested_walk(s))
