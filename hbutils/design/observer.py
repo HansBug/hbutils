5530e7a93c02706f4b0eafd5b78c3546686b5c5c
HansBug <hansbug@buaa.edu.cn>
2022-05-30 21:06:49 +0800

    dev(hansbug): add fix of observer

--- hbutils/design/observer.py
+++ hbutils/design/observer.py
@@ -4,7 +4,7 @@ Overview:
     See `Observer Pattern - Wikipedia <https://en.wikipedia.org/wiki/Observer_pattern>`_.
 """
 from enum import Enum
-from typing import Union, Type, TypeVar, Dict, Callable, Tuple, Set, Any
+from typing import Union, Type, TypeVar, Dict, Callable, Tuple, Any, List
 
 __all__ = [
     'Observable'
@@ -32,6 +32,16 @@ def _get_object_id(obj) -> Tuple[str, int]:
 _EventType = TypeVar('_EventType')
 _SubscriberType = TypeVar('_SubscriberType')
 _CallbackType = TypeVar('_CallbackType', bound=Callable[..., Any])
+
+
+class _CallbackWrapper:
+    def __init__(self, callback):
+        from ..reflection import dynamic_call, sigsupply
+        self.raw = callback
+        self._callback = dynamic_call(sigsupply(callback, lambda x: None))
+
+    def __call__(self, *args, **kwargs):
+        return self._callback(*args, **kwargs)
 
 
 class Observable:
@@ -99,16 +109,43 @@ class Observable:
             {<MyIntEnum.A: 1>: {}, <MyIntEnum.B: 2>: {}}
         """
         events = _auto_members(events)
-        self._event_set: Set[_EventType] = set(events)
-        self._events: Dict[_EventType, Dict[_SubscriberType, _CallbackType]] = {e: {} for e in events}
+        self._observers: Dict[Tuple[str, int], _SubscriberType] = {}
+        self._events: Dict[_EventType, Dict[Tuple[str, int], _CallbackWrapper]] = {e: {} for e in events}
 
-    def get_subscribers(self, event: _EventType) -> Dict[_SubscriberType, _CallbackType]:
+    def subscribers(self, event: _EventType) -> List[_SubscriberType]:
         """
         Get subscribers of the given ``event``.
+
         :param event: Event for querying.
-        :return: A dictionary with subscribers and their callbacks.
+        :return: A list of subscribers.
         """
+        return [self._observers[id_] for id_ in self._get_subscriptions(event).keys()]
+
+    def subscriptions(self, event: _EventType) -> List[Tuple[_SubscriberType, _CallbackType]]:
+        """
+        Get subscriptions of the given ``event``.
+
+        :param event: Event for querying.
+        :return: A list of tuples with subscribers and their callbacks.
+        """
+        return [
+            (self._observers[id_], callback.raw)
+            for id_, callback in self._get_subscriptions(event).items()
+        ]
+
+    def _get_subscriptions(self, event: _EventType) -> Dict[Tuple[str, int], _CallbackWrapper]:
         return self._events[event]
+
+    def _put_subscription(self, event: _EventType, subscriber: _SubscriberType, callback: _CallbackType):
+        subscriber_id = _get_object_id(subscriber)
+        self._get_subscriptions(event)[subscriber_id] = _CallbackWrapper(callback)
+        self._observers[subscriber_id] = subscriber
+
+    def _del_subscription(self, event: _EventType, subscriber: _SubscriberType):
+        try:
+            del self._get_subscriptions(event)[_get_object_id(subscriber)]
+        except KeyError:
+            raise KeyError(subscriber)
 
     def subscribe(self, event: _EventType, subscriber: _SubscriberType,
                   callback: Union[_CallbackType, str, None] = None):
@@ -135,9 +172,7 @@ class Observable:
         if not callable(callback):
             raise TypeError(f'Callback should be callable, but {repr(callback)} found.')
 
-        from ..reflection import dynamic_call, sigsupply
-        self.get_subscribers(event)[_get_object_id(subscriber)] = \
-            dynamic_call(sigsupply(callback, lambda x: None))
+        self._put_subscription(event, subscriber, callback)
 
     def unsubscribe(self, event: _EventType, subscriber: _SubscriberType):
         """
@@ -146,11 +181,7 @@ class Observable:
         :param event: Event to be unsubscribed.
         :param subscriber: Subscriber of this unsubscription.
         """
-        subscriber_id = _get_object_id(subscriber)
-        try:
-            del self.get_subscribers(event)[subscriber_id]
-        except KeyError:
-            raise KeyError(subscriber)
+        self._del_subscription(event, subscriber)
 
     def dispatch(self, event: _EventType):
         """
@@ -158,5 +189,5 @@ class Observable:
 
         :param event: Event to be dispatched.
         """
-        for _, callback in self.get_subscribers(event).items():
+        for _, callback in self._get_subscriptions(event).items():
             callback(event, self)
