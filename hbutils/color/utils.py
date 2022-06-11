343c77240155b328ccf2b207b969a80e7f658d8d
HansBug <hansbug@buaa.edu.cn>
2022-06-11 14:49:33 +0800

    dev(hansbug): add gradient for colors

--- hbutils/color/utils.py
+++ hbutils/color/utils.py
@@ -4,17 +4,26 @@ Overview:
 """
 import math
 import random
-from typing import Iterator
+from typing import Iterator, Union, Sequence, Tuple, Callable
 
 from .model import Color
+from ..algorithm import linear_map
 
 __all__ = [
     'visual_distance',
     'rnd_colors',
+    'linear_gradient',
 ]
 
 
-def visual_distance(c1: Color, c2: Color) -> float:
+def _to_color(color: Color):
+    if isinstance(color, Color):
+        return color
+    else:
+        return Color(color)
+
+
+def visual_distance(c1: Union[Color, str], c2: Union[Color, str]) -> float:
     """
     Overview:
         Get distance of 2 colors.
@@ -29,16 +38,17 @@ def visual_distance(c1: Color, c2: Color) -> float:
     Examples::
         >>> from hbutils.color import visual_distance, Color
         >>> visual_distance(
-        ...     Color.from_hex('#ff0000'),
-        ...     Color.from_hex('#00ff00')
+        ...     '#ff0000',
+        ...     '#00ff00'
         ... )
         2.5495097567963922
         >>> visual_distance(
-        ...     Color.from_hex('#778800'),
-        ...     Color.from_hex('#887700')
+        ...     '#778800',
+        ...     '#887700'
         ... )
         0.16996731711975946
     """
+    c1, c2 = _to_color(c1), _to_color(c2)
     rgb1, rgb2 = c1.rgb, c2.rgb
     rmean = (rgb1.red + rgb2.red) / 2
     dr = rgb1.red - rgb2.red
@@ -138,3 +148,83 @@ def rnd_colors(
                 if try_cnt >= count * 2:
                     min_distance *= lr
                     try_cnt = 0
+
+
+def linear_gradient(colors: Union[Sequence[Union[Color, str]], Sequence[Tuple[float, Union[Color, str]]]]) \
+        -> Callable[[float], Color]:
+    """
+    Overview:
+        Linear gradient of the colors.
+
+    :param colors: Colors to gradient.
+    :return: A mapping function for gradient.
+
+    Examples::
+        - Simple Linear Gradientation
+
+        >>> from hbutils.color import linear_gradient
+        >>>
+        >>> f = linear_gradient(('red', 'yellow', 'lime'))
+        >>> f(0)
+        <Color red>
+        >>> f(0.25)
+        <Color #ff8000>
+        >>> f(1 / 3)
+        <Color #ffaa00>
+        >>> f(0.5)
+        <Color yellow>
+        >>> f(2 / 3)
+        <Color #aaff00>
+        >>> f(0.75)
+        <Color #80ff00>
+        >>> f(1)
+        <Color lime>
+
+        - Complex Linear Gradientation
+
+        >>> f = linear_gradient(((-0.2, 'red'), (0.7, '#ffff0044'), (1.1, 'lime')))
+        >>> f(-0.2)
+        <Color red, alpha: 1.000>
+        >>> f(0)
+        <Color #ff3900, alpha: 0.837>
+        >>> f(0.25)
+        <Color #ff8000, alpha: 0.633>
+        >>> f(1 / 3)
+        <Color #ff9700, alpha: 0.565>
+        >>> f(0.5)
+        <Color #ffc600, alpha: 0.430>
+        >>> f(2 / 3)
+        <Color #fff600, alpha: 0.294>
+        >>> f(0.7)
+        <Color yellow, alpha: 0.267>
+        >>> f(0.75)
+        <Color #dfff00, alpha: 0.358>
+        >>> f(0.8)
+        <Color #bfff00, alpha: 0.450>
+        >>> f(1)
+        <Color #40ff00, alpha: 0.817>
+        >>> f(1.1)
+        <Color lime, alpha: 1.000>
+
+    """
+    try:
+        xys = [(x, _to_color(y)) for x, y in colors]
+    except ValueError:
+        pts = list(colors)
+        n = len(pts)
+        xys = [(i / (n - 1), _to_color(y)) for i, y in enumerate(colors)]
+
+    rmap = linear_map([(x, y.rgb.red) for x, y in xys])
+    gmap = linear_map([(x, y.rgb.green) for x, y in xys])
+    bmap = linear_map([(x, y.rgb.blue) for x, y in xys])
+    if any([y.alpha is not None for _, y in xys]):
+        amap = linear_map([(x, y.alpha if y.alpha is not None else 1.0) for x, y in xys])
+    else:
+        # noinspection PyUnusedLocal
+        def amap(x):
+            return None
+
+    def _gradient(x: float) -> Color:
+        return Color((rmap(x), gmap(x), bmap(x)), amap(x))
+
+    return _gradient
