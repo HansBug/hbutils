b2abfdc75a3ca06baf5bacc1fd664bc84c5d809a
HansBug <hansbug@buaa.edu.cn>
2022-09-09 21:27:14 +0800

    dev(hansbug): add is_binary_file and is_text_file function

---
+++ test/system/filesystem/test_binary.py
@@ -0,0 +1,35 @@
+import pytest
+
+from hbutils.system import is_binary_file, is_text_file
+from ...testings import get_testfile_path
+
+
+@pytest.mark.unittest
+class TestSystemFilesystemBinary:
+    @pytest.mark.parametrize(
+        ['file', 'is_binary'],
+        [
+            ('igm/zip_template-simple.zip', True),
+            ('igm/rar_template-simple.rar', True),
+            ('igm/xztar_template-simple.tar.xz', True),
+            ('igm/LICENSE', False),
+            ('igm/README.md', False),
+            ('empty', False),
+        ]
+    )
+    def test_is_binary_file(self, file, is_binary):
+        assert is_binary_file(get_testfile_path(file)) == is_binary
+
+    @pytest.mark.parametrize(
+        ['file', 'is_text'],
+        [
+            ('igm/zip_template-simple.zip', False),
+            ('igm/rar_template-simple.rar', False),
+            ('igm/xztar_template-simple.tar.xz', False),
+            ('igm/LICENSE', True),
+            ('igm/README.md', True),
+            ('empty', True),
+        ]
+    )
+    def test_is_text_file(self, file, is_text):
+        assert is_text_file(get_testfile_path(file)) == is_text
