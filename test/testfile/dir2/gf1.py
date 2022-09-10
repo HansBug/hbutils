9cb5cbee9fe2447268f254d46327c7036e8ad9f3
HansBug <hansbug@buaa.edu.cn>
2022-09-11 00:18:14 +0800

    dev(hansbug): update mount_pythonpath feature

--- test/testfile/dir2/gf1.py
+++ test/testfile/dir2/gf1.py
@@ -1,1 +1,11 @@
 FIXED = 455
+
+
+def m1():
+    from mth import func3
+    return func3() ** 2.5
+
+
+def m2():
+    from mth import func1, func3
+    return (func3() + func1()) ** 2.1
---
+++ test/testfile/dir2/mth.py
@@ -0,0 +1,6 @@
+def func1():
+    return 23
+
+
+def func3():
+    return 0xff
