74c89abcf7a22d0aee72336eca85cebae131bdb7
HansBug <hansbug@buaa.edu.cn>
2023-01-04 17:33:05 +0800

    dev(hansbug): add utils about pip and requirements

--- hbutils/system/python/package.py
+++ hbutils/system/python/package.py
@@ -1,16 +1,30 @@
+import importlib
+import os
+import pathlib
+import subprocess
+import sys
 from typing import Dict, Optional
+from typing import List
 
+import pkg_resources
 from packaging.version import Version
-from pkg_resources import working_set, parse_version
 
 __all__ = [
     'package_version',
+    'load_req_file', 'pip', 'pip_install',
+    'check_reqs', 'check_req_file',
 ]
 
-PIP_PACKAGES: Dict[str, Version] = {
-    i.key.lower(): i.version
-    for i in working_set
-}
+PIP_PACKAGES: Dict[str, str] = {}
+
+
+def _get_pip_pakages() -> Dict[str, str]:
+    global PIP_PACKAGES
+    if not PIP_PACKAGES:
+        for i in pkg_resources.working_set:
+            PIP_PACKAGES[i.key.lower()] = i.version
+
+    return PIP_PACKAGES
 
 
 def package_version(name: str) -> Optional[Version]:
@@ -32,7 +46,48 @@ def package_version(name: str) -> Optional[Version]:
         None
     """
     _lower_name = name.lower()
-    if _lower_name in PIP_PACKAGES:
-        return parse_version(PIP_PACKAGES[_lower_name])
+    pip_packages = _get_pip_pakages()
+    if _lower_name in pip_packages:
+        return pkg_resources.parse_version(pip_packages[_lower_name])
     else:
         return None
+
+
+def load_req_file(requirements_file: str) -> List[str]:
+    with pathlib.Path(requirements_file).open() as reqfile:
+        return list(map(str, pkg_resources.parse_requirements(reqfile)))
+
+
+def pip(*args, silent: bool = False):
+    with open(os.devnull, 'w') as sout:
+        try:
+            process = subprocess.run(
+                [sys.executable, '-m', 'pip', *args],
+                stdin=sys.stdin if not silent else None,
+                stdout=sys.stdout if not silent else sout,
+                stderr=sys.stderr if not silent else sout,
+            )
+            process.check_returncode()
+        finally:
+            if args and args[0] in {'install', 'uninstall'}:
+                importlib.reload(pkg_resources)
+                global PIP_PACKAGES
+                PIP_PACKAGES.clear()
+
+
+def check_reqs(reqs: List[str]) -> bool:
+    try:
+        pkg_resources.require(reqs)
+    except (pkg_resources.DistributionNotFound, pkg_resources.VersionConflict):
+        return False
+    else:
+        return True
+
+
+def check_req_file(requirements_file: str) -> bool:
+    return check_reqs(load_req_file(requirements_file))
+
+
+def pip_install(reqs: List[str], silent: bool = False, force: bool = False):
+    if force or not check_reqs(reqs):
+        pip('install', *reqs, silent=silent)
--- test/system/python/test_package.py
+++ test/system/python/test_package.py
@@ -1,11 +1,14 @@
-from unittest import mock
+from unittest import mock, skipUnless
 
+import packaging
+import pip as _pip_pkg
 import pytest
 from pkg_resources import parse_version
 
-from hbutils.system import package_version
-from hbutils.system.python.package import PIP_PACKAGES
+from hbutils.system import package_version, load_req_file, check_reqs, check_req_file, pip, pip_install
+from hbutils.testing import capture_output, vpip
 from .test_version import _Version
+from ...testings import get_testfile_path
 
 
 @pytest.mark.unittest
@@ -16,7 +19,48 @@ class TestSystemPythonPackage:
         assert package_version("chardet") < parse_version("5")
         assert package_version("This_is_an_fxxking_name") is None
 
-        with mock.patch.dict(PIP_PACKAGES, {'pip': '19.3.1'}):
+        with mock.patch.dict('hbutils.system.python.package.PIP_PACKAGES', {'pip': '19.3.1'}):
             assert package_version('pip') == parse_version('19.3.1')
             assert package_version('PIP') == parse_version('19.3.1')
             assert package_version('pipxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx') is None
+
+    def test_load_req_file(self):
+        assert load_req_file(get_testfile_path('requirements-1.txt')) == [
+            'chardet<5,>=3.0.4', 'bitmath>=1.3.3.1', 'pytimeparse>=1.1.8',
+            'inflect>=5.2.0', 'packaging>=21.3', 'setuptools>=50.0'
+        ]
+
+    def test_check_reqs(self):
+        assert check_reqs(['packaging>=21.3', 'setuptools>=50.0'])
+        assert not check_reqs(['packaging>=21.3', 'setuptools<48.0'])
+
+    def test_check_req_file(self):
+        assert check_req_file('requirements.txt')
+        assert not check_req_file(get_testfile_path('requirements-2.txt'))
+
+    def test_pip(self):
+        with capture_output() as co:
+            pip('-V')
+        assert _pip_pkg.__version__ in co.stdout
+
+        with capture_output() as co:
+            pip('-V', silent=True)
+        assert not co.stdout
+        assert not co.stderr
+
+        with capture_output() as co:
+            pip('freeze')
+        assert f'packaging=={packaging.__version__}' in co.stdout
+
+    @skipUnless(not vpip('requests'), 'No requests package required.')
+    def test_pip_install(self):
+        try:
+            pip_install(['requests>=2.24'], silent=True)
+            import requests
+            assert vpip('requests') == requests.__version__
+
+            pip('uninstall', '-y', 'requests', silent=True)
+            assert not vpip('requests')
+        finally:
+            if check_reqs(['requests']):
+                pip('uninstall', '-y', 'requests', silent=True)
