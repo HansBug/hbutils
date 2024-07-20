425153efb5bdd6f2fc4ecba2fdef216b26c2662b
HansBug <hansbug@buaa.edu.cn>
2024-07-20 19:17:59 +0800

    dev(hansbug): add unittest

--- test/testing/requires/conftest.py
+++ test/testing/requires/conftest.py
@@ -1,4 +1,5 @@
+import os.path
 import shutil
 
-_GIT_LFS = shutil.which('git_lfs', path='dist')
-_GIT_RAW = shutil.which('git_raw', path='dist')
+_GIT_LFS = shutil.which('git_lfs', path=os.path.abspath('dist'))
+_GIT_RAW = shutil.which('git_raw', path=os.path.abspath('dist'))
