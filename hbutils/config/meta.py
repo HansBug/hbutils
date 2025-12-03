4ee26814c65558ed8e0e55b8ce26e9c6925638ed
HansBug <hansbug@buaa.edu.cn>
2025-12-03 14:02:17 +0800

    dev(hansbug): use version v0.13.1

--- hbutils/config/meta.py
+++ hbutils/config/meta.py
@@ -11,7 +11,7 @@ setup configuration and package distribution.
 __TITLE__ = "hbutils"
 
 #: Version of this project.
-__VERSION__ = "0.13.0"
+__VERSION__ = "0.13.1"
 
 #: Short description of the project, will be included in ``setup.py``.
 __DESCRIPTION__ = 'Some useful functions and classes in Python infrastructure development.'
